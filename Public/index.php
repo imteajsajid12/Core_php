b538e1f0ed1b700b4b4829fdc8d3083ac0d4cee3
MACBOOK <macbook@IMTEAJ-MacBook-Pro.local>
2024-02-01 22:10:17 +0600

    stack queue

--- Public/index.php
+++ Public/index.php
@@ -1,2 +1,28 @@
 <?php
 echo 'Hello World!';
+echo 'Hello World!';
+echo 'Hello World!';
+
+
+
+//write a array and print it
+
+$array = [1, 2, 3, 4, 5];
+echo 'array is '."<br />" ;
+print_r($array);
+echo "<br />";
+
+
+// stack//
+//array_pop($array);
+//print_r($array);
+//array_push($array,10);
+//print_r($array);
+
+//queue
+array_shift($array);
+print_r($array);
+array_unshift($array);
+print_r($array);
+
+
