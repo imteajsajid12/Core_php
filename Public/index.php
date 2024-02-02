27d75de759ede2c1d729b54ee11d26dd8ab1d687
MACBOOK <macbook@IMTEAJ-MacBook-Pro.local>
2024-02-02 11:37:04 +0600

    sql connection

--- Public/index.php
+++ Public/index.php
@@ -1,28 +1,82 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+</head>
+<body>
+<div class="container">
+<h1>Hello, world!</h1>
+<!--input grup  -->
+    <form class="row g-3" action="" method="get">
+        <div class="col-md-6">
+            <label for="inputEmail4" class="form-label">Email</label>
+            <input type="text" name="email" class="form-control" id="inputEmail4">
+        </div>
+        <div class="col-md-6">
+            <label for="inputPassword4" class="form-label">Password</label>
+            <input type="text" name="password" class="form-control" id="inputPassword4">
+        </div>
+
+
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+        </div>
+    </form>
+
 <?php
-echo 'Hello World!';
-echo 'Hello World!';
-echo 'Hello World!';
+
+
+$user= $_GET['email'];
+$passwords = trim(strtolower($_GET['password']));
+
+//mysqli connection
+$servername = "db";
+$username = "user";
+$password = "pass";
+$dbname = "database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+
+//insert data
+$sql = "INSERT INTO users (name, email, password) VALUES ('imteaj', '$user' , '$passwords')";
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully";
+} else {
+    echo "Error inserting data: " . $conn->error;
+}
+
+
+?>
 
 
 
-//write a array and print it
+</div>
 
-$array = [1, 2, 3, 4, 5];
-echo 'array is '."<br />" ;
-print_r($array);
-echo "<br />";
+<!-- Optional JavaScript; choose one of the two! -->
 
+<!-- Option 1: Bootstrap Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-// stack//
-//array_pop($array);
-//print_r($array);
-//array_push($array,10);
-//print_r($array);
-
-//queue
-array_shift($array);
-print_r($array);
-array_unshift($array);
-print_r($array);
-
-
+<!-- Option 2: Separate Popper and Bootstrap JS -->
+<!--
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+-->
+</body>
+</html>
